bba4b5e819aa592507238011c9f0a50a633bc79c
https://git.melissadata.com/savannah <switham.gca@gmail.com>
2021-02-17 16:54:55 -0500

    Add project files.

---
+++ UserWebApp/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+namespace UserWebApp.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<UserWebApp.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "UserWebApp.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(UserWebApp.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
